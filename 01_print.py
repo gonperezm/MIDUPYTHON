6169f3d852239256fa31363828b8fa8596e3244e
gonperezm <gonzaloperezmontalvo@gmail.com>
2025-01-17 12:29:26 -0300

    python_curso

---
+++ 01_print.py
@@ -0,0 +1,8 @@
+print("hola mundo") 
+print('esto funciona con comillas simples')
+
+print("python", "es", "brutal", sep="-")
+
+print("python", "es", "magnifico", sep="-", end="!")
+
+print("va todo en una sola linea")
