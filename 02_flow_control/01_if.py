a7e6655aaabfe864bfa6de94bac786a8dc86ad46
gonperezm <gonzaloperezmontalvo@gmail.com>
2025-01-27 11:15:40 -0300

    update

--- 02_flow_control/01_if.py
+++ 02_flow_control/01_if.py
@@ -91,3 +91,99 @@ elif tiene_dinero:
         print("puedes entrar a la disco")
 else:
     print("quedate en casa")          
+
+
+
+
+
+print("\n La condición ternaria:") 
+#es una forma concisa de un if-else en una line ade codigo
+# [codigo si cumple la condicion] if [la condicion] else [codigo si no cumple]
+edad = 17
+
+mensaje = "Es mayor de edad" if edad >= 18 else "Es menor de edad"
+print(mensaje) 
+
+
+###
+# EJERCICOS
+###
+
+# Ejercicio 1: Determinar el mayor de dos números
+# Pide al usuario que introduzca dos números y muestra un mensaje
+# indicando cuál es mayor o si son iguales
+
+
+
+print("-------------------------EJERCICIOS----------------------------------")
+# print("\ndeterminar cual es mayor o si son iguales")
+# numero1=input("ingresa el primer numero\n")
+# numero2=input("ingresa el segundo numero\n")
+
+# if numero1 < numero2:
+#     print(f"el primer numero {numero1} es menor que el segundo numero {numero2}")
+# elif numero1 == numero2:
+#         print(f"el primer numero {numero1} es igual que el segundo numero {numero2}")
+# else:
+#     print(f"el primer numero {numero1} es mayor que el segundo numero {numero2}")
+    
+
+# Ejercicio 2: Calculadora simple
+# Pide al usuario dos números y una operación (+, -, *, /)
+# Realiza la operación y muestra el resultado (maneja la división entre zero)
+
+
+
+# print("CALCULADORA, ingresa los dos numeros y el signo de la operacion\n")
+# numero1=int(input("ingresa el primer valor\n"))
+# numero2=int(input("ingresa el segundo valor\n"))
+# operacion=input("ingresa el simbolo de la operacion siendo estos +,-,*,/ \n")
+
+# if operacion == "+":
+#     print(f"la suma de {numero1} + {numero2} es igual a: ", numero1+numero2)
+# elif operacion == "-":
+#     print(f"la resta de {numero1} - {numero2} es igual a: ", numero1-numero2)
+# elif operacion == "*":
+#     print(f"la multi de {numero1} por {numero2} es igual a: ", numero1*numero2)  
+# else:
+#     print(f"la div de {numero1} / {numero2} es igual a: ", numero1/numero2)      
+
+
+
+
+# Ejercicio 3: Año bisiesto
+# Pide al usuario que introduzca un año y determina si es bisiesto.
+# Un año es bisiesto si es divisible por 4, excepto si es divisible por 100 pero no por 400.
+
+
+# print("determinemos si es bisiento un año")
+# anio=int(input("introduce un año que calculemos\n"))
+# if (anio % 4 == 0 and anio % 100 != 0) or anio % 400 == 0:
+#     print(f"{anio} es un año bisiesto.")
+# else:
+#     print(f"{anio} no es un año bisiesto.")
+
+
+
+# Ejercicio 4: Categorizar edades
+# Pide al usuario que introduzca una edad y la clasifique en:
+# - Bebé (0-2 años)
+# - Niño (3-12 años)
+# - Adolescente (13-17 años)
+# - Adulto (18-64 años)
+# - Adulto mayor (65 años o más)
+
+
+# print("dime tu edad y te dire en que categoria estas")
+# edad=int(input("que edad tienes?\n"))
+
+# if edad >0 and edad<2:
+#     print("eres un Bebé!")
+# elif edad >3 and edad<12:
+#     print("eres un Niño!")
+# elif edad >13 and edad<17:
+#     print("eres un Adolescente!")
+# elif edad >18 and edad<64:
+#     print("eres un Adulto!")            
+# else:
+#     print("eres Adulto mayor")   
--- 02_flow_control/02_booleans.py
+++ 02_flow_control/02_booleans.py
@@ -4,6 +4,9 @@
 # Fundamentales para el control de flujo y la logica de programacion.
 ###
 
+import os 
+os.system("clear")
+
 print(" Valores booleanos basicos:")
 print(True)
 print(False)
@@ -16,3 +19,30 @@ print("5 == 5:", 5==5) #True (igualdad)
 print("5 != 3:", 5 !=3) #True ( desigualdad)
 print("5 >= 5:", 5>=5) #True (mayor o igual que)
 print("5 <= 3:", 5>=3) #False (menor o igual que)
+
+
+print("comparacion de cadenas:")
+print("manzana < pera: ", "manzana"<"pera") #true cuidad q da true porque compara caracter por caracter 
+print("'Hola' == 'hola'", 'Hola' == 'hola') #false, porq es sensible los upper cap
+ 
+
+print("\nTablas de la verdad")
+print("and:")
+print("A        B       A and B")
+print("True   True      ", True and True)
+print("True   False     ", True and False)
+print("False   True     ", False and True)
+print("False   False    ", False and False)
+
+
+print("\nor:")
+print("A        B       A or B")
+print("True   True      ", True and True)
+print("True   False     ", True and False)
+print("False   True     ", False and True)
+print("False   False    ", False and False)
+
+print("\nnot:")
+print("A        not A")
+print("True   ", not True)
+print("False  ", not False)
